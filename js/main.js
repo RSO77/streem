af275fc4e34c38ce3b9f2e72f32bedb18184f9a9
rso <Ostapchenko.roma@gmail.com>
2018-09-20 11:30:36 +0300

    20.09

--- js/main.js
+++ js/main.js
@@ -7,17 +7,37 @@ window.onload = function () {
     var button3 = $('#button3_js');
     var num = document.getElementById('number_js');
     var context = canvas.getContext('2d');
+    var section = document.querySelector(".section");
+    var main = document.querySelector(".my_main");
+    var container = document.querySelector(".container");
 
-    //
-    // $.getJSON("test.json", function (data) {
-    //     alert(data);
-    // });
+    $.getJSON("js/test.json", function(json) {
+        console.log(json);
+        if($.isEmptyObject(json) == 0) content(json);
+    });
+
+    function content(json) {
+        // установка цвета беграунда
+        main.style.backgroundColor = json.main_theme.background.color;
+
+        // установка ширины и высоты
+        section.style.height = json.main_theme.height + "px";
+        section.style.width = json.main_theme.width + "px";
+        // layout
+        // if(json.layout.orientation == "portrait"){$(".bg-block").addClass('bg-block2');}
+
+        // проверка наличия ada_theme
+        if(json.ada_theme){
+
+        }
+    }
 
     var i=1;
     function score (){
         num.innerHTML = i;
         $("#number_js").addClass('transit');
         $('#number_js').css('font-size', 170+'px');
+        videoCam();
         setTimeout(function () {
             if (i < 3){
                 i += 1;
@@ -26,10 +46,9 @@ window.onload = function () {
                 $("#number_js").removeClass('transit');
                 $('#number_js').css('font-size', 17+'px');}, "900");
         if (i == 3){
-            video.src = 'http://192.168.101.147/addons/get-camera-stream.php';
             setTimeout(function () {
                 num.innerHTML = '';
-                context.drawImage(video, 0, 0, 600, 450);
+                img();
                 $('.stream_js').addClass('display');
                 $('.photo_js').removeClass('display');
             },900);}};
@@ -45,14 +64,15 @@ window.onload = function () {
     }
 
     $(".bg-block").click( function () {
-        $(".overlay").removeClass('display');
-        $(".bg-block").addClass('display');
-        $(this).removeClass('display');
-        $('.bg-photo').width('700');
-        button1.removeClass('display');
-        button2.removeClass('display');
-        button3.removeClass('display');
-        score();
+        if($('.bg-photo').width() == 250){
+            $(".overlay").removeClass('display');
+            $(".bg-block").addClass('display');
+            $(this).removeClass('display');
+            $('.bg-photo').width('700');
+            button1.removeClass('display');
+            button2.removeClass('display');
+            button3.removeClass('display');
+            score();}
     });
 
     button1.click( function () {
@@ -61,21 +81,14 @@ window.onload = function () {
 
 
     function img() {
-        if(screen.width > 767)
-            context.drawImage(video, 0, 0, 600, 450);
-        else
-            context.drawImage(video, 0, 0, 450, 600);
-    };
+        context.drawImage(video, 0, 0);}
     function videoCam() {
-        $('.photo_js').addClass('display');
-        $('.stream_js').removeClass('display');
-        video.src = 'http://192.168.101.147/addons/get-camera-stream.php';
-    }
+        video.src = 'http://192.168.101.147/addons/get-camera-stream.php';}
 
 
     button2.click(function () {
         img();
-        $('.stream').addClass('display');
+        // $('.stream').addClass('display');
         $('.photo').removeClass('display');
     });
     // console.log('что-то не так с видеостримом или пользователь запретил его использовать :P');
